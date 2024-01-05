7b5a9f37ea47c0f0ee372d07f3d210d9bb0752b6
Robert Tanzola <r.c.tanzola@gmail.com>
2024-01-05 01:38:15 -0500

    css update navbar transparency on scroll, width

--- src/components/projects/ProjectDetail.js
+++ src/components/projects/ProjectDetail.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams} from 'react-router-dom';
 import { details } from './data/details.js';
-// import Vimeo from '@u-wave/react-vimeo';
 import './ProjectDetail.css';
 
 function clamp(num, min, max) {
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './App.js';
@@ -10,5 +11,5 @@ window.addEventListener("load", (event) => {
 });
 
 const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(<App />);
